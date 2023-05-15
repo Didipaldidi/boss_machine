b8a32360794cf3873b3ec9b090b684ec9cb28a4c
Valdi <valdilaurentius@gmail.com>
2023-05-14 21:49:56 -0700

    fxing the api.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
 const cors = require('cors');
-app.use(cors);
+app.use(cors());
 
 // Add middware for parsing request bodies here:
 const bodyParser = require('body-parser');
-app.use(bodyParser);
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
@@ -23,6 +23,7 @@ app.use('/api', apiRouter);
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-  app.listen(PORT);
-  console.log(`> Server is listening on portL ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 }
